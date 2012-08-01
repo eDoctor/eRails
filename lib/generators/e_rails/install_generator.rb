866e285224f9c5ebd8352d7d379d5da04883b6c0
wo_is神仙 <i@mrzhang.me>
2012-08-01 19:58:03 +0800

    clean & fix

--- lib/generators/e_rails/install_generator.rb
+++ lib/generators/e_rails/install_generator.rb
@@ -7,21 +7,24 @@ module ERails
 
       def copy_config_file
         copy_file 'Gemfile', 'Gemfile'
+        copy_file 'gitignore', '.gitignore'
         
-        copy_file 'config/app_config.rb', 'config/initializers/app_config.rb'
-        copy_file 'config/slim.rb', 'config/initializers/slim.rb'
+        copy_file 'app_config.rb', 'config/initializers/app_config.rb'
+        copy_file 'slim.rb', 'config/initializers/slim.rb'
         
-        copy_file 'config/config.yml.example', 'config/config.yml.example'
-        copy_file 'config/config.yml.example', 'config/config.yml'
-        copy_file 'config/database.yml.example', 'config/database.yml.example'
-        copy_file 'config/database.yml.example', 'config/database.yml'
+        copy_file 'config.yml.example', 'config/config.yml.example'
+        copy_file 'database.yml.example', 'config/database.yml.example'
         
-        gsub_file 'config/environments/production.rb', /config.assets.compile = false/, 'config/assets.compile = true'
+        gsub_file 'config/environments/production.rb', /config.assets.compile = false/, 'config.assets.compile = true'
+        gsub_file 'config/environments/production.rb', /# config.assets.precompile += %w( search.js )/, 'config.assets.precompile = [/[^_]\.css$/]'
        
-        insert_into_file 'config/environments/production.rb', "  config.sass.line_comments = false\n", :after => /configure do\n/
-        insert_into_file 'config/environments/production.rb', "  config.assets.precompile = [/[^_]\.css$/]\n", :after => /configure do\n/
+        insert_into_file 'config/environments/development.rb', "  config.sass.line_comments = false\n", :after => /configure do\n/
 
-        run 'bundle install'
+        run 'bundle install && bundle exec compass init rails'
+        run 'rm -r app/assets/images/*'
+        run 'rm -r app/assets/javascripts/*'
+        run 'rm -r app/assets/stylesheets/*'
+        run 'rm -r config/database.yml'
       end
     end
   end
