df75774ca2ba76dbd3b579b243bc264d87315b1e
wo_is神仙 <i@mrzhang.me>
2012-08-01 20:43:28 +0800

    :trollface:

--- lib/generators/e_rails/install_generator.rb
+++ lib/generators/e_rails/install_generator.rb
@@ -16,9 +16,9 @@ module ERails
         copy_file 'database.yml.example', 'config/database.yml.example'
         
         gsub_file 'config/environments/production.rb', /config.assets.compile = false/, 'config.assets.compile = true'
-        insert_into_file 'config/environments/production.rb', "\n  config.assets.precompile = [/[^_]\.css$/]", :before => /\n# config.assets.precompile/
+        insert_into_file 'config/environments/production.rb', "\n  config.assets.precompile = [/[^_]\.css$/]\n", :after => /search.js )\n/
        
-        insert_into_file 'config/environments/development.rb', "\n  config.sass.line_comments = false", :after => /debug = true\n/
+        insert_into_file 'config/environments/development.rb', "\n  config.sass.line_comments = false\n", :after => /debug = true\n/
 
         run 'bundle install && bundle exec compass init rails'
         run 'rm -r app/assets/images/*'
