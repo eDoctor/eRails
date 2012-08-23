d0d9ff318de29140e51af9fb357711d41160fe5c
wo_is神仙 <i@mrzhang.me>
2012-08-23 17:15:56 +0800

    review path

--- lib/e_rails/path.rb
+++ lib/e_rails/path.rb
@@ -3,9 +3,7 @@ module Sass::Script::Functions
   # instead of image-url(), but with the `assets_dir`
   def path(source, only_path = Sass::Script::Bool.new(false))
     source = source.value.path
-
-    source = "url(/assets/#{source})" if !only_path.to_bool
-
+    source = "url(/assets/#{source})" unless only_path.to_bool
     Sass::Script::String.new(source)
   end
 
@@ -17,11 +15,7 @@ class String
   # instead of xxx_path(), but with the `assets_dir`
   def path
     source = self
-
-    if source[0..7] != "/assets/" && source[0..3] != "http"
-      source = APP_CONFIG["assets_dir"] + "/" + source
-    end
-
+    source = File.join(APP_CONFIG['assets_dir'], source) unless source.start_with?('/assets/', 'http')
     source
   end
 
