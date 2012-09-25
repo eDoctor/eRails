5c00a409fc4418a6aae43a1ef7530951d90708c1
wo_is神仙 <i@mrzhang.me>
2012-09-25 16:32:16 +0800

    modify generator

--- README.md
+++ README.md
@@ -10,10 +10,26 @@ cd app_name/
 
 ## Step 2
 
+使用下面的代码替换掉默认的 `Gemfile` 内容
+
 ```ruby
-echo "source 'http://ruby.taobao.org'" > Gemfile
-echo "gem 'rails', '~> 3.2.6'" >> Gemfile
-echo "gem 'e_rails', :git => 'git://github.com/eDoctor/eRails.git'" >> Gemfile
+source 'http://ruby.taobao.org'
+
+gem 'rails', '~> 3.2.8'
+gem 'e_rails', :git => 'git://github.com/eDoctor/eRails.git'
+gem 'slim-rails'
+
+group :assets do
+  gem 'compass-rails'
+  gem 'sass-rails'
+end
+
+########## ☟ Sort by letter ☟ ##########
+# gem 'capistrano'
+# gem 'mongoid'
+# gem 'mysql2'
+# gem 'rails_emoji'
+# gem 'rails_kindeditor'
 ```
 
 ## Step 3
--- lib/generators/e_rails/install_generator.rb
+++ lib/generators/e_rails/install_generator.rb
@@ -5,19 +5,19 @@ module ERails
         File.expand_path('../../../../templates', __FILE__)
       end
 
-      def copy_config_file
-        copy_file 'Gemfile', 'Gemfile'
+      def do_something
         copy_file 'gitignore', '.gitignore'
-        
-        copy_file 'app_config.rb', 'config/initializers/app_config.rb'
-        copy_file 'slim.rb', 'config/initializers/slim.rb'
-        
         copy_file 'config.yml.example', 'config/config.yml.example'
         copy_file 'database.yml.example', 'config/database.yml.example'
-        
+        copy_file 'app_config.rb', 'config/initializers/app_config.rb'
+        copy_file 'slim.rb', 'config/initializers/slim.rb'
+
+        copy_file 'zh_cn_datetime.yml', 'config/locales/zh_cn_datetime.yml'
+        copy_file 'en_datetime.yml', 'config/locales/en_datetime.yml'
+        gsub_file 'config/application.rb', /# config.i18n.default_locale = :de/, 'config.i18n.default_locale = :zh_cn'
+
         gsub_file 'config/environments/production.rb', /config.assets.compile = false/, 'config.assets.compile = true'
         insert_into_file 'config/environments/production.rb', "\n  config.assets.precompile += [/[^_]\.css$/]\n", :after => /\( search.js \)\n/
-       
         insert_into_file 'config/environments/development.rb', "\n  config.sass.line_comments = false\n", :after => /debug = true\n/
 
         run 'bundle install && bundle exec compass init rails'
