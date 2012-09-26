ee878b3ab40205803cab3f9179a3f308a704acc3
wo_is神仙 <i@mrzhang.me>
2012-09-26 16:35:03 +0800

    tweak

--- lib/generators/e_rails/install_generator.rb
+++ lib/generators/e_rails/install_generator.rb
@@ -5,6 +5,15 @@ module ERails
         File.expand_path('../../../../templates', __FILE__)
       end
 
+      def init_compass
+        run 'bundle exec compass init rails'
+      end
+
+      def clean
+        run 'rm -r app/assets/images/* app/assets/javascripts/* app/assets/stylesheets/*'
+        run 'rm -r .gitignore app/views/layouts/application.html.erb config/database.yml public/index.html'
+      end
+
       def copy_files
         copy_file 'gitignore', '.gitignore'
         copy_file 'config.yml.example', 'config/config.yml.example'
@@ -16,22 +25,13 @@ module ERails
       def modify_environments
         gsub_file 'config/environments/production.rb', /config.assets.compile = false/, 'config.assets.compile = true'
         insert_into_file 'config/environments/production.rb', "\n  config.assets.precompile += [/[^_]\.css$/]", :after => /\( search.js \)/
-        insert_into_file 'config/environments/development.rb', "\n  config.sass.line_comments = false\n", :before => /end/
+        insert_into_file 'config/environments/development.rb', "\n  config.sass.line_comments = false\n", :after => /debug = true\n/
       end
 
       def set_locales_for_date_time_helper
         copy_file 'zh_cn_datetime.yml', 'config/locales/zh_cn_datetime.yml'
         copy_file 'en_datetime.yml', 'config/locales/en_datetime.yml'
         gsub_file 'config/application.rb', /# config.i18n.default_locale = :de/, 'config.i18n.default_locale = :zh_cn'
-      end
-
-      def init_compass
-        run 'bundle exec compass init rails'
-      end
-
-      def clean
-        run 'rm -r app/assets/images/*; rm -r app/assets/javascripts/*; rm -r app/assets/stylesheets/*'
-        run 'rm -r app/views/layouts/application.html.erb; rm -r config/database.yml; rm -r public/index.html'
       end
     end
   end
