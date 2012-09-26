a3e473abe03f037eb32474765e9ea6756f2916bb
wo_is神仙 <i@mrzhang.me>
2012-09-26 10:29:39 +0800

    classify

--- lib/generators/e_rails/install_generator.rb
+++ lib/generators/e_rails/install_generator.rb
@@ -5,22 +5,31 @@ module ERails
         File.expand_path('../../../../templates', __FILE__)
       end
 
-      def do_something
+      def copy_configuration
         copy_file 'gitignore', '.gitignore'
         copy_file 'config.yml.example', 'config/config.yml.example'
         copy_file 'database.yml.example', 'config/database.yml.example'
         copy_file 'app_config.rb', 'config/initializers/app_config.rb'
         copy_file 'slim.rb', 'config/initializers/slim.rb'
+      end
 
-        copy_file 'zh_cn_datetime.yml', 'config/locales/zh_cn_datetime.yml'
-        copy_file 'en_datetime.yml', 'config/locales/en_datetime.yml'
-        gsub_file 'config/application.rb', /# config.i18n.default_locale = :de/, 'config.i18n.default_locale = :zh_cn'
-
+      def modify_environments
         gsub_file 'config/environments/production.rb', /config.assets.compile = false/, 'config.assets.compile = true'
         insert_into_file 'config/environments/production.rb', "\n  config.assets.precompile += [/[^_]\.css$/]\n", :after => /\( search.js \)\n/
         insert_into_file 'config/environments/development.rb', "\n  config.sass.line_comments = false\n", :after => /debug = true\n/
+      end
 
+      def set_locales_for_date_time_helper
+        copy_file 'zh_cn_datetime.yml', 'config/locales/zh_cn_datetime.yml'
+        copy_file 'en_datetime.yml', 'config/locales/en_datetime.yml'
+        gsub_file 'config/application.rb', /# config.i18n.default_locale = :de/, 'config.i18n.default_locale = :zh_cn'
+      end
+
+      def init_compass
         run 'bundle exec compass init rails'
+      end
+
+      def clean
         run 'rm -r app/assets/images/*; rm -r app/assets/javascripts/*; rm -r app/assets/stylesheets/*'
         run 'rm -r app/views/layouts/application.html.erb; rm -r config/database.yml; rm -r public/index.html'
       end
