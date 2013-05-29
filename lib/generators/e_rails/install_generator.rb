ea00e9a2c8764f540339eafac087c4a50301e413
张君君 <i@mrzhang.me>
2013-05-29 14:23:05 +0800

    add assets path

--- lib/generators/e_rails/install_generator.rb
+++ lib/generators/e_rails/install_generator.rb
@@ -10,14 +10,14 @@ module ERails
       end
 
       def clean
-        run 'rm -r app/assets/images/* app/assets/javascripts/* app/assets/stylesheets/*'
-        run 'rm -r .gitignore app/views/layouts/application.html.erb config/database.yml public/index.html'
+        run 'rm -r app/assets/images/* app/assets/javascripts/* app/assets/stylesheets/* app/views/layouts/*'
+        run 'rm -r .gitignore config/database.yml public/index.html'
       end
 
       def copy_files
         copy_file 'gitignore', '.gitignore'
-        copy_file 'config.yml.example', 'config/config.yml.example'
-        copy_file 'database.yml.example', 'config/database.yml.example'
+        copy_file 'config.example.yml', 'config/config.example.yml'
+        copy_file 'database.example.yml', 'config/database.example.yml'
         copy_file 'app_config.rb', 'config/initializers/app_config.rb'
         copy_file 'slim.rb', 'config/initializers/slim.rb'
       end
@@ -26,6 +26,10 @@ module ERails
         gsub_file 'config/environments/production.rb', /config.assets.compile = false/, 'config.assets.compile = true'
         insert_into_file 'config/environments/production.rb', "\n  config.assets.precompile += [/[^_]\.css$/]", :after => /\( search.js \)/
         insert_into_file 'config/environments/development.rb', "\n  config.sass.line_comments = false\n", :after => /debug = true\n/
+      end
+
+      def add_assets_path
+        insert_into_file 'config/application.rb', "    config.assets.paths << \"\#{Rails.root}/app/assets/fonts\"\n", :after => /enabled = true\n/
       end
 
       def set_locales_for_date_time_helper
