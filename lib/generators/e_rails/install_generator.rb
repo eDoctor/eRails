17e6789bc6cf2666e4676b88767781df4e281a99
张君君 <i@mrzhang.me>
2013-08-29 16:04:56 +0800

    update locale pkg

--- lib/generators/e_rails/install_generator.rb
+++ lib/generators/e_rails/install_generator.rb
@@ -15,15 +15,17 @@ module ERails
       end
 
       def copy_files
-        copy_file 'gitignore', '.gitignore'
-        copy_file 'config.example.yml', 'config/config.example.yml'
-        copy_file 'app_config.rb', 'config/initializers/app_config.rb'
-        copy_file 'slim.rb', 'config/initializers/slim.rb'
-        copy_file 'layout.slim', 'app/views/layouts/application.slim'
-        copy_file 'zh_cn_datetime.yml', 'config/locales/zh_cn_datetime.yml'
-        copy_file 'en_datetime.yml', 'config/locales/en_datetime.yml'
-        copy_file 'zh_cn_button_tag.yml', 'config/locales/zh_cn_button_tag.yml'
-        copy_file 'en_button_tag.yml', 'config/locales/en_button_tag.yml'
+        copy_file 'gitignore',                '.gitignore'
+        copy_file 'layout.slim',              'app/views/layouts/application.slim'
+        copy_file 'config.example.yml',       'config/config.example.yml'
+        copy_file 'app_config.rb',            'config/initializers/app_config.rb'
+        copy_file 'slim.rb',                  'config/initializers/slim.rb'
+        copy_file 'button_tag_zh_cn.yml',     'config/locales/button_tag_zh_cn.yml'
+        copy_file 'button_tag_en.yml',        'config/locales/button_tag_en.yml'
+        copy_file 'datetime_zh_cn.yml',       'config/locales/datetime_zh_cn.yml'
+        copy_file 'datetime_en.yml',          'config/locales/datetime_en.yml'
+        copy_file 'flash_message_zh_cn.yml',  'config/locales/flash_message_zh_cn.yml'
+        copy_file 'flash_message_en.yml',     'config/locales/flash_message_en.yml'
       end
 
       def modify_environments
