5eb2cf9f6a78e2b65fbc28d0b509f7d0650d638b
张君君 <i@mrzhang.me>
2013-08-29 16:06:12 +0800

    update app_config

--- templates/app_config.rb
+++ templates/app_config.rb
@@ -1,7 +1,14 @@
 root = Rails.root.to_s
+env = Rails.env
 
-APP_CONFIG = YAML.load_file("#{root}/config/config.yml")[Rails.env]
 RELEASE_VERSION = File.basename(root)
+APP_CONFIG = {}
+JS_VERSION = {}
 
-path = Rails.env == "production" ? "tmp" : "app/assets/javascripts/#{APP_CONFIG["assets_dir"]}"
-JS_VERSION = JSON(File.read(File.join(root, path, "package.json")))["devDependencies"]
+if File.file?(config_yml = File.join(root, "config/config.yml"))
+  APP_CONFIG = YAML.load_file(config_yml)[env]
+end
+
+if File.file?(package_json = File.join(root, env == "production" ? "tmp" : "app/assets/javascripts/#{APP_CONFIG["assets_dir"]}", "package.json"))
+  JS_VERSION = JSON(File.read(package_json))["devDependencies"]
+end
