fdf99295d2f3e3ad30221b950d5170a618e712ca
张君君 <i@mrzhang.me>
2013-09-02 16:04:30 +0800

    fix

--- lib/generators/e_rails/install_generator.rb
+++ lib/generators/e_rails/install_generator.rb
@@ -15,19 +15,9 @@ module ERails
       end
 
       def copy_files
-        copy_file 'gitignore', '.gitignore'
-        copy_file 'layout.slim', 'app/views/layouts/application.slim'
-        copy_file 'config.example.yml', 'config/config.example.yml'
-
-        copy_file 'initializers/app_config.rb', 'config/initializers/app_config.rb'
-        copy_file 'initializers/slim.rb',       'config/initializers/slim.rb'
-
-        copy_file 'locales/button_tag/en.yml',    'config/locales/button_tag/en.yml'
-        copy_file 'locales/button_tag/zh_cn.yml', 'config/locales/button_tag/zh_cn.yml'
-        copy_file 'locales/datetime/en.yml',      'config/locales/datetime/en.yml'
-        copy_file 'locales/datetime/zh_cn.yml',   'config/locales/datetime/zh_cn.yml'
-        copy_file 'locales/flash/en.yml',         'config/locales/flash/en.yml'
-        copy_file 'locales/flash/zh_cn.yml',      'config/locales/flash/zh_cn.yml'
+        FileUtils.cp 'gitignore', '.gitignore'
+        FileUtils.cp 'layout.slim', 'app/views/layouts/application.slim'
+        FileUtils.cp_r %w( initializers/ locales/ config.example.yml ), 'config/'
       end
 
       def modify_env_files
@@ -38,7 +28,7 @@ module ERails
 
       def modify_configs
         insert_into_file 'config/application.rb', "    config.assets.paths << \"\#{Rails.root}/app/assets/fonts\"\n", after: /enabled = true\n/
-        insert_into_file 'config/application.rb', "    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml')]", before: /\n    # config\.i18n\.default_locale/
+        insert_into_file 'config/application.rb', "    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml')]\n", before: /    # config\.i18n\.default_locale/
         gsub_file 'config/application.rb', /# config\.i18n\.default_locale = :de/, 'config.i18n.default_locale = :zh_cn'
       end
     end
