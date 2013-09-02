ce896ec318dc8503b050d6eb8751ce0ce5017e9a
张君君 <i@mrzhang.me>
2013-09-03 01:26:23 +0800

    update geturl helper

--- lib/e_rails/custom_methods.rb
+++ lib/e_rails/custom_methods.rb
@@ -39,38 +39,41 @@ module ERails
 
     # 处理当前页 URI 的参数并返回新的 URI
     def geturl(*args)
-      opts = args.extract_options!
+      options = args.extract_options!
       url, params = request.fullpath.split('?')
       url = args.first unless args.first.blank?
 
-      if params.blank?
-        params = {}
+      params = if params.blank?
+        {}
       else
-        params = params.split('&').collect{|x| x.split('=')}.collect{|x| {x[0].to_s => x[1].to_s}}.inject(:merge)
+        params.split('&').collect{ |x| x.split('=') }
+          .collect{ |x| { x[0].to_s => x[1].to_s } }
+          .inject(:merge)
       end
 
       # 保留参数(其余移除)
-      if !(keep_params = opts[:keep_params]).blank?
-        temp_params = {}
-        params.each do |k,v|
-          temp_params[k] = v if keep_params.index k
-        end
-        params = temp_params
+      unless (keep_params = options[:keep_params]).blank?
+        keep_params.collect! { |x| x.to_s }
+        params.slice! *keep_params
       end
 
       # 移除参数
-      if (remove_params = opts[:remove_params]).is_a? Array
-        if remove_params.empty? # 是空数组则移除全部
-          params = {}
+      if (remove_params = options[:remove_params]).is_a? Array
+        params = if remove_params.empty? # 是空数组则移除全部
+          {}
         else
-          remove_params.each { |i| params.delete i }
+          remove_params.collect! { |x| x.to_s }
+          params.except *remove_params
         end
       end
 
       # 替换/新增参数
-      opts[:edit_params].each { |k,v| params[k.to_s] = v.to_s } unless opts[:edit_params].blank?
+      params.merge! (options[:edit_params] || {}).stringify_keys
 
-      return [url, params.collect{|k,v| k.to_s + '=' + v.to_s}.join('&')].join('?')
+      [
+        url,
+        params.collect{ |k, v| k.to_s + '=' + v.to_s }.sort.join('&')
+      ].join('?')
     end
 
   end
