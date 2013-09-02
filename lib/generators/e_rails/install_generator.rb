9e0f2337c3c0db559729ddc8053c1049664c9091
张君君 <i@mrzhang.me>
2013-09-02 15:41:06 +0800

    tweak templates

--- lib/generators/e_rails/install_generator.rb
+++ lib/generators/e_rails/install_generator.rb
@@ -15,30 +15,30 @@ module ERails
       end
 
       def copy_files
-        copy_file 'gitignore',                '.gitignore'
-        copy_file 'layout.slim',              'app/views/layouts/application.slim'
-        copy_file 'config.example.yml',       'config/config.example.yml'
-        copy_file 'app_config.rb',            'config/initializers/app_config.rb'
-        copy_file 'slim.rb',                  'config/initializers/slim.rb'
-        copy_file 'button_tag_zh_cn.yml',     'config/locales/button_tag_zh_cn.yml'
-        copy_file 'button_tag_en.yml',        'config/locales/button_tag_en.yml'
-        copy_file 'datetime_zh_cn.yml',       'config/locales/datetime_zh_cn.yml'
-        copy_file 'datetime_en.yml',          'config/locales/datetime_en.yml'
-        copy_file 'flash_message_zh_cn.yml',  'config/locales/flash_message_zh_cn.yml'
-        copy_file 'flash_message_en.yml',     'config/locales/flash_message_en.yml'
+        copy_file 'gitignore', '.gitignore'
+        copy_file 'layout.slim', 'app/views/layouts/application.slim'
+        copy_file 'config.example.yml', 'config/config.example.yml'
+
+        copy_file 'initializers/app_config.rb', 'config/initializers/app_config.rb'
+        copy_file 'initializers/slim.rb',       'config/initializers/slim.rb'
+
+        copy_file 'locales/button_tag/en.yml',    'config/locales/button_tag/en.yml'
+        copy_file 'locales/button_tag/zh_cn.yml', 'config/locales/button_tag/zh_cn.yml'
+        copy_file 'locales/datetime/en.yml',      'config/locales/datetime/en.yml'
+        copy_file 'locales/datetime/zh_cn.yml',   'config/locales/datetime/zh_cn.yml'
+        copy_file 'locales/flash/en.yml',         'config/locales/flash/en.yml'
+        copy_file 'locales/flash/zh_cn.yml',      'config/locales/flash/zh_cn.yml'
       end
 
-      def modify_environments
+      def modify_env_files
         gsub_file 'config/environments/production.rb', /config\.assets\.compile = false/, 'config.assets.compile = true'
         gsub_file 'config/environments/production.rb', /# config\.assets\.precompile \+= %w\( search\.js \)/, 'config.assets.precompile += [Proc.new { |path| File.basename(path) =~ /^[^_][a-z0-9-]+\.css$/ }]'
         insert_into_file 'config/environments/development.rb', "\n  config.sass.debug_info = true\n", before: /end/
       end
 
-      def add_assets_path
+      def modify_configs
         insert_into_file 'config/application.rb', "    config.assets.paths << \"\#{Rails.root}/app/assets/fonts\"\n", after: /enabled = true\n/
-      end
-
-      def set_default_locale
+        insert_into_file 'config/application.rb', "    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml')]", before: /\n    # config\.i18n\.default_locale/
         gsub_file 'config/application.rb', /# config\.i18n\.default_locale = :de/, 'config.i18n.default_locale = :zh_cn'
       end
     end
